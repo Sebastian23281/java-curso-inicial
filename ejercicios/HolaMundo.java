12d50e988fe919bd5b44b2d270b8f97a90d53755
Sebastian23281 <sebastianhemard@gmail.com>
2024-07-17 14:07:05 -0400

    Update HolaMundo.java

--- ejercicios/HolaMundo.java
+++ ejercicios/HolaMundo.java
@@ -1,5 +1,5 @@
 public class HolaMundo {
     public static void main(String[] args) {
-        System.out.println("¡Hola, Sebastian Hemard!");
+        System.out.println("¡Hola, Mundo!");
     }
 }
